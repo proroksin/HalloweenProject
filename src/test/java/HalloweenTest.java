456c3c40f55c5fb36b0a927b55ee30d6c37c265f
proroksin <sinaverkemalov@gmail.com>
2020-10-31 15:48:15 -0500

    updatubt HalloweenTest class from magic branch

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,4 +1,5 @@
 public class HalloweenTest {
+    //add a comment, message from magic branch
     public static void main(String[] args) {
         //I am about to givee everyone candy
         //new add and
